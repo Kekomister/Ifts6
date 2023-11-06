6f0dbe75fa53a5b5be4d410686205200df36a9ff
Kekomister <jeremyg1999@example.com>
2023-11-06 18:05:24 -0300

    Hora

--- ifts6/src/back/controllers/publicacion.ts
+++ ifts6/src/back/controllers/publicacion.ts
@@ -20,6 +20,9 @@ const fs = require('fs');
 // id_Publicacion, titulo, descripcion, imagen, fecha_Publicacion, id_Usuario, id_Sector
 //  int,           varchar,varchar,   varbinary,   date,             int,        int
 
+dayjs.extend(utc);
+dayjs.extend(timezone);
+
 const getPublicacion = (async (req: Request, res: Response) => {
     console.log("HELLO");
     try {
@@ -86,44 +89,43 @@ function convertirAImagenes(array: any) {
 }
 
 
-dayjs.extend(utc);
-dayjs.extend(timezone);
-
 const createPublicacion = (async (req: Request, res: Response) => {
     let bod = req.body;
     console.log(req.body);
     
     let dat = new Date().toISOString();
     const timeZone: string = 'America/Buenos_Aires';
-    const formattedDate = dayjs(dat).tz(timeZone).format('DD/MM/YYYY');
-    //console.log(formattedDate); // Salida: 06/11/2023
-    try {
-        handleMultipartData(req, res, async (err: { message: any; }) => {
-            if (err) {
-                //res.json({ msgs: err.message });
-            }
-            let pool = await new sql.ConnectionPool(config).connect();
+    // const formattedDate = dayjs(dat).tz(timeZone).format('DD/MM/YYYY');
+    const formattedDate = dayjs(dat).tz(timeZone);
+    console.log(formattedDate); // Salida: 06/11/2023
+    res.send();
+    // try {
+    //     handleMultipartData(req, res, async (err: { message: any; }) => {
+    //         if (err) {
+    //             //res.json({ msgs: err.message });
+    //         }
+    //         let pool = await new sql.ConnectionPool(config).connect();
 
-            let img = await convertirABuffer(req.files);
+    //         let img = await convertirABuffer(req.files);
 
-            let query =
-                `INSERT INTO Publicaciones 
-            (titulo, descripcion, imagen, fecha_Publicacion, id_Usuario, id_Sector)
-            VALUES (@titulo, @desc, @img, @fecha, @user, @sector)`
-            let result = await pool.request()
-                .input('titulo', sql.VarChar, bod.titulo)
-                .input('desc', sql.VarChar, bod.descripcion)
-                .input('img', sql.VarBinary, img)
-                .input('fecha', sql.Date, formattedDate)
-                .input('user', sql.Int, bod.id_Usuario)
-                .input('sector', sql.Int, bod.id_Sector)
-                .query(query);
-            vaciarCarpeta();
-            res.send(result);
-        });
-    } catch (e) {
-        console.log(e);
-    }
+    //         let query =
+    //             `INSERT INTO Publicaciones 
+    //         (titulo, descripcion, imagen, fecha_Publicacion, id_Usuario, id_Sector)
+    //         VALUES (@titulo, @desc, @img, @fecha, @user, @sector)`
+    //         let result = await pool.request()
+    //             .input('titulo', sql.VarChar, bod.titulo)
+    //             .input('desc', sql.VarChar, bod.descripcion)
+    //             .input('img', sql.VarBinary, img)
+    //             .input('fecha', sql.Date, formattedDate)
+    //             .input('user', sql.Int, bod.id_Usuario)
+    //             .input('sector', sql.Int, bod.id_Sector)
+    //             .query(query);
+    //         vaciarCarpeta();
+    //         res.send(result);
+    //     });
+    // } catch (e) {
+    //     console.log(e);
+    // }
 })
 
 async function vaciarCarpeta() {
