e5fc88010f84bb8472bb1fe98d7da1664d35617d
AntoBalqui <miller_rodrigo@hotmail.com>
2023-11-10 20:14:48 -0300

    nueva página /creditos

---
+++ ifts6/src/app/pages/creditos/creditos.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+import { ConexionService } from 'src/app/services/conexion.service';
+
+@Component({
+  selector: 'app-creditos',
+  templateUrl: './creditos.component.html',
+  styleUrls: ['./creditos.component.css']
+})
+export class CreditosComponent {
+  
+}
